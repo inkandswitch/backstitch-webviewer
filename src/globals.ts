843ff07e890fc0904e2db7ac8057fea8546eb8a2
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-08-26 19:09:23 -0700

    docker

--- src/globals.ts
+++ src/globals.ts
@@ -1,12 +1,18 @@
 let apiUrl = import.meta.env.VITE_BACKSTITCH_API_URL
 
 if (apiUrl == undefined || "") {
-    console.error("API URL must be defined by setting VITE_BACKSTITCH_API_URL in the `npm build` command!");
+    // @ts-ignore
+    apiUrl = window.BACKSTITCH_API_URL
+}
+
+if (apiUrl == undefined || "") {
+    console.error("API URL must be defined by setting VITE_BACKSTITCH_API_URL in the `npm build` command!\
+        Alternatively, you can override it once the project is built by altering config.js to set window.BACKSTITCH_API_URL.");
     apiUrl = "/api";
 }
 
 // https://stackoverflow.com/questions/29855098/is-there-a-built-in-javascript-function-similar-to-os-path-join/
-function pathJoin(parts){
+function pathJoin(parts: string[]){
     const separator = '/';
     parts = parts.map((part, index)=>{
         if (index) {
