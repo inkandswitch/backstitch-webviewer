32fc7291cbc3dd94b84ceab345a3d6937d0da184
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-08-26 20:16:24 -0700

    re-add godot stuff direct to public

--- test/test_serializer.ts
+++ test/test_serializer.ts
@@ -2,7 +2,7 @@ import { serializeGodotScene } from "../src/godot_serializer";
 
 function test() {
   const fs = require("fs");
-  const structured_content = JSON.parse(fs.readFileSync("test.json", "utf8"));
+  const structured_content = JSON.parse(fs.readFileSync("test/test.json", "utf8"));
   const serialized = serializeGodotScene(structured_content);
   // console.log(serialized);
   fs.writeFileSync("test_files/test.tscn", serialized);
